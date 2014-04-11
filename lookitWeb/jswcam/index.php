22d08858bf845d52beda56f6783687e93303bb31
Kim Scott <lookit@mit.edu>
2014-04-11 14:42:46 -0400

    meta tags on index.php

--- lookitWeb/jswcam/index.php
+++ lookitWeb/jswcam/index.php
@@ -18,6 +18,8 @@ if(!isset($_SESSION['user']['id'])){
     <title> Lookit: Online experiments from the Early Childhood Cognition Lab </title>
     <link rel="shortcut icon" type="image/x-icon" href="img/remy4.ico">    
 	
+	
+	
     <link rel="stylesheet/less" type="text/css" href="fragments/styles.less"></link>
     <link rel="stylesheet/less" type="text/css" href="bootstrap/less/bootstrap.less"></link>
     <link rel="stylesheet/css" type="text/css" href="static/css/styles.css"></link>
