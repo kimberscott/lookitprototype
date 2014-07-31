72fa135d0d42d359e4f8decccffda941903862ce
Kim Scott <lookit@mit.edu>
2014-07-31 16:03:13 -0400

    remove extra echo statements in counterbalancing

--- lookitWeb/jswcam/counterbalance.php
+++ lookitWeb/jswcam/counterbalance.php
@@ -17,13 +17,10 @@ function check_conditions($experiment_id,$string) {
   $experiments = $m->users->experiment_age; // Mongo collection called 'account'; documents in this 
   $thisExp = $experiments->findOne(array('experiment_id' => $experiment_id));
   $nConds = $thisExp['INCLUDED'];
-  
-  echo json_encode($nConds);
 
   $min_part = min($nConds); 
   $needy_conds = array_keys($nConds, $min_part);
 
-  echo json_encode($needy_conds);
   $c = $needy_conds[rand(0, count($needy_conds)-1)];
 
   return array('condition'=>$c);
