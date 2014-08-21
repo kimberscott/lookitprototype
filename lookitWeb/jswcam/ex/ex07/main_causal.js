f29aa4aa78f54c9b7a104e18e11685def9dc4f51
Kim Scott <lookit@mit.edu>
2014-08-21 15:37:45 -0400

    Revert "switch record_whole_study to true in causaldomains for testing"
    This reverts commit b2bb98f9b335e70d03d88581777107afa66199e0.

--- lookitWeb/jswcam/ex/ex07/main_causal.js
+++ lookitWeb/jswcam/ex/ex07/main_causal.js
@@ -138,18 +138,12 @@ function startExperiment(condition, box) {
 		
         // Allow the user to end the experiment by pressing 'Home' or 'End' keys.
 	     document.addEventListener('keydown', getKeyCode, false);
-             /*
-                  if (record_whole_study) {
-                      jswcam.startRecording();
-                       addEvent( {'type': 'startRecording'});
-                   }
-              */
-           // Start the experiment
 	     advanceSegment();
-	 }else{
-
-    sleep(4000);
-    show_cam_widget("position","webcamdiv");
+	 }
+	 else{ // Recording whole study: use show_cam_widget to load the cam/mic widget.
+	    // Once it's loaded, connected_mic_cam() will be called to remove the modal pop-up and begin loading.
+		sleep(4000);
+		show_cam_widget("position","webcamdiv");
 	 }
 }
 
@@ -226,10 +220,10 @@ function generateHtml(segmentName){
 				
 			case "positioning":
                     if (!record_whole_study) {
-				show_cam("position","webcamdiv");
-		    }else{
+						show_cam("position","webcamdiv");
+					}else{
 		                show_getting_setup_widget();
-		    }
+					}
 			
 			case "instructions":
 			
@@ -416,8 +410,8 @@ function generateHtml(segmentName){
 		goFullscreen($('#baseline')[0]);
 	} else if (segmentName=='formPoststudy') {
 	    if(!record_whole_study){
-		$("#flashplayer").remove();
-		$("#widget_holder").css("display","none"); // Removes the widget at the end of the experiment
+			$("#flashplayer").remove();
+			$("#widget_holder").css("display","none"); // Removes the widget at the end of the experiment
 	    }
 		leaveFullscreen();
 	}
