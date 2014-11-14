aca08169a46c4e5ae8ce761e44859fe41a5ef1b9
mit-github-deploy <mit-github-deploy@webintensive.com>
2014-11-14 10:26:36 +0530

    Removing the average brightness and current FPS message.
    Removed the display of the the following from the Bootbox:
    1. Average Brightness.
    2. Current FPS.

--- lookitWeb/jswcam/index.js
+++ lookitWeb/jswcam/index.js
@@ -213,7 +213,6 @@ var page = (function() {
 		$('.btn-stop').css("display","none");
 		$('.btn-record').css("display","none");
 		$('.btn-record').attr('disabled', 'disabled');
-		$("#brightness_message").remove();
 	    consent_recording_completed = 0;
 		show_cam("consent","cam_setup");
 
