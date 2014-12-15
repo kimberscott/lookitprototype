a8e8f9aa78310cbc1e156c7f33288860dfd00314
mit-github-deploy <mit-github-deploy@webintensive.com>
2014-12-15 13:31:52 +0530

    Merging production branch and Changes for unsupported browser
    Changes added for:
    1. Merging production branch
    2. Alert for unsupported browser

--- lookitWeb/jswcam/login/myfunc.js
+++ lookitWeb/jswcam/login/myfunc.js
@@ -451,7 +451,12 @@ function login(html,expr,obje){
                         if(obje){
                             $(".bootbox").remove();
                             $(".modal-backdrop").remove();
-                            select_child(expr,obje);
+                            if(check_browser_support()){
+                                select_child(expr,obje);
+                            }
+                        }
+                        else{
+                            check_browser_support();
                         }
                         return true;
                     }
@@ -586,6 +591,9 @@ function display_modal(){
         'label': 'No',
         "class": 'btn-danger',
         'callback': function() {
+            $(".bootbox").remove();
+            $(".modal-backdrop").remove();
+            check_browser_support();
             return true;
         }
     },
@@ -594,6 +602,9 @@ function display_modal(){
         'class': 'btn-primary btn-stop',
         'callback': function() {
             show_demographic();
+            $(".bootbox").remove();
+            $(".modal-backdrop").remove();
+            check_browser_support();
         }
     }
     ]);
@@ -624,9 +635,13 @@ function select_child(expr,obje){
     new_page += "<p>You will select a privacy level for your recordings at the end of the study.  Unless you allow the recordings to be used, no video except for the consent video will be viewed by anyone.</p>";
     show_childs(new_page,expr,obje);
 }
-
+var object_new;
+var experiment_new;
 // Function to create the child and privacy selection pop-up
 function show_childs(html,expr,obje){
+    object_new = obje;
+    experiment_new = expr;
+
      bootbox.dialog(html,[
     {
         'label': 'Cancel',
@@ -1034,8 +1049,6 @@ function send_post_data(post_data){
 
     // As long as the user did not withdraw, also do a final DB update.
     if ('continue' in post_data) {
-        experiment.browser_info = browserInfo.browser +' '+ browserInfo.browserVersion;
-        experiment.OS_info = browserInfo.os +' '+ browserInfo.osVersion;
         $.ajax({
             'type': 'POST',
             'url': './user.php',
@@ -1070,179 +1083,32 @@ function avarageBrightness(brightness){
 
 }
 
-(function (window) {
-    {
-       var unknown = 'Unknown';
-
-        // screen
-        var screenSize = '';
-        if (screen.width) {
-            width = (screen.width) ? screen.width : '';
-            height = (screen.height) ? screen.height : '';
-            screenSize += '' + width + " x " + height;
-        }
-
-        //browser
-        var nVer = navigator.appVersion;
-        var nAgt = navigator.userAgent;
-        var browser = navigator.appName;
-        var version = '' + parseFloat(navigator.appVersion);
-        var majorVersion = parseInt(navigator.appVersion, 10);
-        var nameOffset, verOffset, ix;
-
-        // Opera
-        if ((verOffset = nAgt.indexOf('Opera')) != -1) {
-            browser = 'Opera';
-            version = nAgt.substring(verOffset + 6);
-            if ((verOffset = nAgt.indexOf('Version')) != -1) {
-                version = nAgt.substring(verOffset + 8);
-            }
-        }
-        // MSIE
-        else if ((verOffset = nAgt.indexOf('MSIE')) != -1) {
-            browser = 'Microsoft Internet Explorer';
-            version = nAgt.substring(verOffset + 5);
-        }
-
-        //IE 11 no longer identifies itself as MS IE, so trap it
-        //http://stackoverflow.com/questions/17907445/how-to-detect-ie11
-        else if ((browser == 'Netscape') && (nAgt.indexOf('Trident/') != -1)) {
-
-            browser = 'Microsoft Internet Explorer';
-            version = nAgt.substring(verOffset + 5);
-            if ((verOffset = nAgt.indexOf('rv:')) != -1) {
-                version = nAgt.substring(verOffset + 3);
-            }
-
-        }
-
-        // Chrome
-        else if ((verOffset = nAgt.indexOf('Chrome')) != -1) {
-            browser = 'Chrome';
-            version = nAgt.substring(verOffset + 7);
-        }
-        // Safari
-        else if ((verOffset = nAgt.indexOf('Safari')) != -1) {
-            browser = 'Safari';
-            version = nAgt.substring(verOffset + 7);
-            if ((verOffset = nAgt.indexOf('Version')) != -1) {
-                version = nAgt.substring(verOffset + 8);
-            }
-
-            // Chrome on iPad identifies itself as Safari. Actual results do not match what Google claims
-            //  at: https://developers.google.com/chrome/mobile/docs/user-agent?hl=ja
-            //  No mention of chrome in the user agent string. However it does mention CriOS, which presumably
-            //  can be keyed on to detect it.
-            if (nAgt.indexOf('CriOS') != -1) {
-                //Chrome on iPad spoofing Safari...correct it.
-                browser = 'Chrome';
-                //Don't believe there is a way to grab the accurate version number, so leaving that for now.
-            }
-        }
-        // Firefox
-        else if ((verOffset = nAgt.indexOf('Firefox')) != -1) {
-            browser = 'Firefox';
-            version = nAgt.substring(verOffset + 8);
-        }
-        // Other browsers
-        else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) < (verOffset = nAgt.lastIndexOf('/'))) {
-            browser = nAgt.substring(nameOffset, verOffset);
-            version = nAgt.substring(verOffset + 1);
-            if (browser.toLowerCase() == browser.toUpperCase()) {
-                browser = navigator.appName;
-            }
-        }
-        // trim the version string
-        if ((ix = version.indexOf(';')) != -1) version = version.substring(0, ix);
-        if ((ix = version.indexOf(' ')) != -1) version = version.substring(0, ix);
-        if ((ix = version.indexOf(')')) != -1) version = version.substring(0, ix);
-
-        majorVersion = parseInt('' + version, 10);
-        if (isNaN(majorVersion)) {
-            version = '' + parseFloat(navigator.appVersion);
-            majorVersion = parseInt(navigator.appVersion, 10);
-        }
-
-        // mobile version
-        var mobile = /Mobile|mini|Fennec|Android|iP(ad|od|hone)/.test(nVer);
-
-        // cookie
-        var cookieEnabled = (navigator.cookieEnabled) ? true : false;
-
-        if (typeof navigator.cookieEnabled == 'undefined' && !cookieEnabled) {
-            document.cookie = 'testcookie';
-            cookieEnabled = (document.cookie.indexOf('testcookie') != -1) ? true : false;
-        }
-
-        // system
-        var os = unknown;
-        var clientStrings = [
-            {s:'Windows 3.11', r:/Win16/},
-            {s:'Windows 95', r:/(Windows 95|Win95|Windows_95)/},
-            {s:'Windows ME', r:/(Win 9x 4.90|Windows ME)/},
-            {s:'Windows 98', r:/(Windows 98|Win98)/},
-            {s:'Windows CE', r:/Windows CE/},
-            {s:'Windows 2000', r:/(Windows NT 5.0|Windows 2000)/},
-            {s:'Windows XP', r:/(Windows NT 5.1|Windows XP)/},
-            {s:'Windows Server 2003', r:/Windows NT 5.2/},
-            {s:'Windows Vista', r:/Windows NT 6.0/},
-            {s:'Windows 7', r:/(Windows 7|Windows NT 6.1)/},
-            {s:'Windows 8.1', r:/(Windows 8.1|Windows NT 6.3)/},
-            {s:'Windows 8', r:/(Windows 8|Windows NT 6.2)/},
-            {s:'Windows NT 4.0', r:/(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/},
-            {s:'Windows ME', r:/Windows ME/},
-            {s:'Android', r:/Android/},
-            {s:'Open BSD', r:/OpenBSD/},
-            {s:'Sun OS', r:/SunOS/},
-            {s:'Linux', r:/(Linux|X11)/},
-            {s:'iOS', r:/(iPhone|iPad|iPod)/},
-            {s:'Mac OS X', r:/Mac OS X/},
-            {s:'Mac OS', r:/(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/},
-            {s:'QNX', r:/QNX/},
-            {s:'UNIX', r:/UNIX/},
-            {s:'BeOS', r:/BeOS/},
-            {s:'OS/2', r:/OS\/2/},
-            {s:'Search Bot', r:/(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/}
-        ];
-        for (var id in clientStrings) {
-            var cs = clientStrings[id];
-            if (cs.r.test(nAgt)) {
-                os = cs.s;
-                break;
-            }
-        }
-
-        var osVersion = unknown;
-
-        if (/Windows/.test(os)) {
-            osVersion = /Windows (.*)/.exec(os)[1];
-            os = 'Windows';
-        }
-
-        switch (os) {
-            case 'Mac OS X':
-                osVersion = /Mac OS X (10[\.\_\d]+)/.exec(nAgt)[1];
-                break;
-
-            case 'Android':
-                osVersion = /Android ([\.\_\d]+)/.exec(nAgt)[1];
-                break;
-
-            case 'iOS':
-                osVersion = /OS (\d+)_(\d+)_?(\d+)?/.exec(nVer);
-                osVersion = osVersion[1] + '.' + osVersion[2] + '.' + (osVersion[3] | 0);
-                break;
-
-        }
+function audioVideoData(audioData,videoData){
+    if((audioData < 100 || videoData < 100) && $('body').hasClass('modal-open')){
+        hide_cam('consent');
+        $(".bootbox").remove();
+        $(".modal-backdrop").remove();
+        object_new.loadExperiment(experiment_new, '.content_pane');
+        $("#message").after("<span class='error' style='color:red;'>It seems that either your video or audio was missing in the recorded message, please try again.</span>");
     }
+    else{
+        done = 1;
+        $('.btn-send').attr('disabled', false);
+        $('.waiting').remove();
+        return done;
+    }
+}
 
-    window.browserInfo = {
-        screen: screenSize,
-        browser: browser,
-        browserVersion: version,
-        mobile: mobile,
-        os: os,
-        osVersion: osVersion,
-        cookies: cookieEnabled
-    };
-}(this));
+function check_browser_support(){
+    var browser = (new WhichBrowser()).browser.name.toString();
+    var os = (new WhichBrowser()).browser.name.toString();
+    console.log(browser +"==>"+ os);
+
+    if($.inArray(browser,unsupportedBrowsers) >= 0 && $.inArray(os,unsupportedOS) >= 0){
+        var html = "This is not a supported browser. To participate in an experiment please access using any of the following browsers: Chrome, Firefox, or Internet Explorer 11+";
+        bootbox.alert(html);
+    }
+    else{
+        return true;
+    }
+}
