4518fc04eb76de7e8fa028d716e03b02b398d014
Kim Scott <kimscott@mit.edu>
2015-05-07 16:26:15 -0400

    Routine update of needed conditions for novel verbs based on consent videos received

--- lookitWeb/jswcam/counterbalance.php
+++ lookitWeb/jswcam/counterbalance.php
@@ -16,8 +16,10 @@ function check_conditions($experiment_id,$string) {
   	if (strcmp($experiment_id, "testimony") == 0) { // Manual CB for 3-year-olds in Testimony
   		$needed = array(0,8,1,9,2,26,18,2,26,19,19,3,6,6,14,14,22,30,4,12,12,20,28,21,21,13);
     	$c = $needed[rand(0, count($needed)-1)];
-	} elseif (strcmp($experiment_id, "novelverbs") == 0) { // Manual CB for novelverbs, 5/1/15
-	  	$needed = array(0,0,1,2,3,6,6,8,9,10,12,13,13,14,15,16,17,19,21,22,22,23,24,25,26,26,27,27,28,29,30);
+	} elseif (strcmp($experiment_id, "novelverbs") == 0) { // Manual CB for novelverbs
+		// Needed: 0,0,1,2,3,6,6,8,9,10,12,13,13,14,15,16,17,19,21,22,22,23,24,25,26,26,27,27,28,29,30,
+		// Removed some we might have on 5/7/15
+	  	$needed = array(0,0,1,3,6,8,10,12,13,14,15,21,23,24,25,26,26);
     	$c = $needed[rand(0, count($needed)-1)];
 	} elseif (strcmp($experiment_id, "oneshot") == 0) { // Manual CB for oneshot, 5/1/15
 	  	$needed = array(1,2,2,5,7,7,7,7);
