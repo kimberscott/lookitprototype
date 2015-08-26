c1da4267c137548eda09843b8148098ec6fdc9be
Kim Scott <kimscott@mit.edu>
2015-08-26 19:54:10 -0400

    update testimony counterbalancing again

--- lookitWeb/jswcam/counterbalance.php
+++ lookitWeb/jswcam/counterbalance.php
@@ -14,7 +14,7 @@ include("./php/config.php");
 function check_conditions($experiment_id,$string) {  
   
   	if (strcmp($experiment_id, "testimony") == 0) { // Manual CB for 3-year-olds in Testimony
-  		$needed = array(0,20);
+  		$needed = array(14,14,12,20,26);
     	$c = $needed[rand(0, count($needed)-1)];
 	} elseif (strcmp($experiment_id, "novelverbs") == 0) { // Manual CB for novelverbs
 	  	$needed = array(1,9,12,16,17,22,27,28,29);
